3bf9d8164253cc5e4b465542298dbabd587aa695
Janley Crsiologo <crisoja@oocl.com>
2021-08-16 21:17:08 +0800

    [ReactCounter][Janley] feat: implement multiple counter

---
+++ src/components/CounterGroup.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Counter from "./Counter";
+
+function CounterGroup(props) {
+   const counterList = generateCounterList();
+
+   function generateCounterList() {
+    return new Array(props.counterSize).fill(Date.now());
+}
+
+    return(
+        <div>
+            { counterList.map((item, index) => (
+             <Counter key={item + index}></Counter>
+            ))}
+        </div>
+    );
+}
+
+export default CounterGroup;
---
+++ src/components/CounterSizeGenerator.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import "../styles/CounterSizeGenerator.css"
+
+function CounterSizeGenerator(props) {
+    
+    const [size, setSize]=useState(0);
+
+    function handleChangeSize(event) {
+        setSize(parseInt(event.target.value));
+        props.updateCounterSize(parseInt(event.target.value));
+    }
+
+    return(
+        <div>
+            <span>Size:</span>
+            <input type="number" min="0" value={size} onChange={handleChangeSize}></input>
+        </div>
+    );
+}
+
+export default CounterSizeGenerator;
