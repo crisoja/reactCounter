1c469f15ef28cfe43437c210a552c967769e292c
Janley Crsiologo <crisoja@oocl.com>
2021-08-17 10:08:05 +0800

    [ReactCounter][Janley] feat: implement handling for deleting size when size is 0

--- src/components/CounterSizeGenerator.js
+++ src/components/CounterSizeGenerator.js
@@ -1,13 +1,13 @@
 import { useState } from "react";
-import "../styles/CounterSizeGenerator.css"
+//import "../styles/CounterSizeGenerator.css"
 
 function CounterSizeGenerator(props) {
     
     const [size, setSize]=useState(0);
 
     function handleChangeSize(event) {
-        setSize(parseInt(event.target.value));
-        props.updateCounterSize(parseInt(event.target.value));
+        const size = event.target.value.length === 0 ? 0 : parseInt(event.target.value)
+        setSize(parseInt(size));
     }
 
     return(
